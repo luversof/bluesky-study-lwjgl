723a90422819a04f3bc909fb7057177efcab94e1
bluesky <luversof@hanmail.net>
2022-05-09 02:58:16 +0900

    9장 Texture Loading in LWJGL3 강의

--- src/main/java/io/github/luversof/study/lwjgl/main/domain/LevelEditorScene.java
+++ src/main/java/io/github/luversof/study/lwjgl/main/domain/LevelEditorScene.java
@@ -4,6 +4,8 @@ import static org.lwjgl.opengl.GL11.GL_FLOAT;
 import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
 import static org.lwjgl.opengl.GL11.GL_UNSIGNED_INT;
 import static org.lwjgl.opengl.GL11.glDrawElements;
+import static org.lwjgl.opengl.GL13.GL_TEXTURE0;
+import static org.lwjgl.opengl.GL13.glActiveTexture;
 import static org.lwjgl.opengl.GL15.GL_ARRAY_BUFFER;
 import static org.lwjgl.opengl.GL15.GL_ELEMENT_ARRAY_BUFFER;
 import static org.lwjgl.opengl.GL15.GL_STATIC_DRAW;
@@ -16,6 +18,7 @@ import static org.lwjgl.opengl.GL20.glVertexAttribPointer;
 import static org.lwjgl.opengl.GL30.glBindVertexArray;
 import static org.lwjgl.opengl.GL30.glGenVertexArrays;
 
+
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 
@@ -23,16 +26,17 @@ import org.joml.Vector2f;
 import org.lwjgl.BufferUtils;
 
 import io.github.luversof.study.lwjgl.main.renderer.Shader;
+import io.github.luversof.study.lwjgl.main.renderer.Texture;
 import io.github.luversof.study.lwjgl.util.TimeUtil;
 
 public class LevelEditorScene extends Scene {
 	
 	private float[] vertexArray = {
-			// position				//color
-			100.5f, 0.5f, 0.0f,		1.0f, 0.0f, 0.0f, 1.0f,	// Bottom right
-			0.5f, 100.5f, 0.0f,		0.0f, 1.0f, 0.0f, 1.0f,	// Top left
-			100.5f, 100.5f, 0.0f,		0.0f, 0.0f, 1.0f, 1.0f,	// Top right
-			0.5f, 0.5f, 0.0f,		1.0f, 1.0f, 0.0f, 1.0f,	// Bottom right
+			// position				//color						// UV Coordinates
+			100.5f, 0.5f, 0.0f,		1.0f, 0.0f, 0.0f, 1.0f,		1, 1,	// Bottom right	0
+			0.5f, 100.5f, 0.0f,		0.0f, 1.0f, 0.0f, 1.0f,		0, 0,	// Top left		1	
+			100.5f, 100.5f, 0.0f,	0.0f, 0.0f, 1.0f, 1.0f,		1, 0,	// Top right	2
+			0.5f, 0.5f, 0.0f,		1.0f, 1.0f, 0.0f, 1.0f,		0, 1,	// Bottom left	3
 	};
 	
 	// IMPORTANT Must be in counter-clockwise order
@@ -49,6 +53,7 @@ public class LevelEditorScene extends Scene {
 	private int vaoID, vboID, eboID;
 	
 	private Shader defaultShader;
+	private Texture testTexture;
 
 	public LevelEditorScene() {
 
@@ -59,6 +64,7 @@ public class LevelEditorScene extends Scene {
 		this.camera = new Camera(new Vector2f());
 		defaultShader = new Shader("src/main/resources/assets/shaders/default.glsl");
 		defaultShader.compile();
+		testTexture = new Texture("src/main/resources/assets/images/testImage.jpg");
 		
 		// ================================================================
 		// Generate VAO, VBO, and EBO buffer objects, and send to GPU
@@ -86,14 +92,16 @@ public class LevelEditorScene extends Scene {
 		// Add the vertex attribute pointers
 		int positionsSize = 3;
 		int colorSize = 4;
-		int floatSizeBytes = 4;
-		int vertexSizeBytes = (positionsSize + colorSize) * floatSizeBytes;
+		int uvSize = 2;
+		int vertexSizeBytes = (positionsSize + colorSize + uvSize) * Float.BYTES;
 		glVertexAttribPointer(0, positionsSize, GL_FLOAT, false, vertexSizeBytes, 0);
 		glEnableVertexAttribArray(0);
 		
-		glVertexAttribPointer(1, colorSize, GL_FLOAT, false, vertexSizeBytes, positionsSize * floatSizeBytes);
+		glVertexAttribPointer(1, colorSize, GL_FLOAT, false, vertexSizeBytes, positionsSize * Float.BYTES);
 		glEnableVertexAttribArray(1);
 		
+		glVertexAttribPointer(2, uvSize, GL_FLOAT, false, vertexSizeBytes, (positionsSize + colorSize) * Float.BYTES);
+		glEnableVertexAttribArray(2);
 	}
 	
 	
@@ -103,6 +111,12 @@ public class LevelEditorScene extends Scene {
 		camera.position.x -= dt * 50.0f;
 		
 		defaultShader.use();
+		
+		// Upload texture to shader
+		defaultShader.uploadTexture("TEX_SAMPLER", 0);
+		glActiveTexture(GL_TEXTURE0);
+		testTexture.bind();
+		
 		defaultShader.uploadMat4f("uProjection", camera.getProjectionMatrix());
 		defaultShader.uploadMat4f("uView", camera.getViewMatrix());
 		defaultShader.uploadFloat("uTime", TimeUtil.getTime());
